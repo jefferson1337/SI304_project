5508236c4bd186a921308973c58c376d170e16fa
jefferson1337 <jeffersonp_p@hotmail.com>
2016-05-20 21:06:09 -0300

    calculadora

---
+++ calculadora_basic/calculadora_basic/Calculadora.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalculadora
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMultiplicacao = New System.Windows.Forms.Button()
+        Me.btnDivisao = New System.Windows.Forms.Button()
+        Me.btnSubtracao = New System.Windows.Forms.Button()
+        Me.btnSoma = New System.Windows.Forms.Button()
+        Me.btnNum9 = New System.Windows.Forms.Button()
+        Me.btnNum8 = New System.Windows.Forms.Button()
+        Me.btnNum7 = New System.Windows.Forms.Button()
+        Me.btnNum6 = New System.Windows.Forms.Button()
+        Me.btnNum5 = New System.Windows.Forms.Button()
+        Me.btnNum4 = New System.Windows.Forms.Button()
+        Me.btnNum3 = New System.Windows.Forms.Button()
+        Me.btnNum2 = New System.Windows.Forms.Button()
+        Me.btnNum1 = New System.Windows.Forms.Button()
+        Me.btnNum0 = New System.Windows.Forms.Button()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.btnLimpar = New System.Windows.Forms.Button()
+        Me.btnVirgula = New System.Windows.Forms.Button()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnMultiplicacao
+        '
+        Me.btnMultiplicacao.Location = New System.Drawing.Point(144, 56)
+        Me.btnMultiplicacao.Name = "btnMultiplicacao"
+        Me.btnMultiplicacao.Size = New System.Drawing.Size(38, 29)
+        Me.btnMultiplicacao.TabIndex = 0
+        Me.btnMultiplicacao.Text = "*"
+        Me.btnMultiplicacao.UseVisualStyleBackColor = True
+        '
+        'btnDivisao
+        '
+        Me.btnDivisao.Location = New System.Drawing.Point(144, 126)
+        Me.btnDivisao.Name = "btnDivisao"
+        Me.btnDivisao.Size = New System.Drawing.Size(38, 29)
+        Me.btnDivisao.TabIndex = 1
+        Me.btnDivisao.Text = "/"
+        Me.btnDivisao.UseVisualStyleBackColor = True
+        '
+        'btnSubtracao
+        '
+        Me.btnSubtracao.Location = New System.Drawing.Point(144, 91)
+        Me.btnSubtracao.Name = "btnSubtracao"
+        Me.btnSubtracao.Size = New System.Drawing.Size(38, 29)
+        Me.btnSubtracao.TabIndex = 2
+        Me.btnSubtracao.Text = "-"
+        Me.btnSubtracao.UseVisualStyleBackColor = True
+        '
+        'btnSoma
+        '
+        Me.btnSoma.Location = New System.Drawing.Point(144, 162)
+        Me.btnSoma.Name = "btnSoma"
+        Me.btnSoma.Size = New System.Drawing.Size(38, 29)
+        Me.btnSoma.TabIndex = 3
+        Me.btnSoma.Text = "+"
+        Me.btnSoma.UseVisualStyleBackColor = True
+        '
+        'btnNum9
+        '
+        Me.btnNum9.Location = New System.Drawing.Point(100, 56)
+        Me.btnNum9.Name = "btnNum9"
+        Me.btnNum9.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum9.TabIndex = 4
+        Me.btnNum9.Text = "9"
+        Me.btnNum9.UseVisualStyleBackColor = True
+        '
+        'btnNum8
+        '
+        Me.btnNum8.Location = New System.Drawing.Point(56, 56)
+        Me.btnNum8.Name = "btnNum8"
+        Me.btnNum8.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum8.TabIndex = 5
+        Me.btnNum8.Text = "8"
+        Me.btnNum8.UseVisualStyleBackColor = True
+        '
+        'btnNum7
+        '
+        Me.btnNum7.Location = New System.Drawing.Point(12, 56)
+        Me.btnNum7.Name = "btnNum7"
+        Me.btnNum7.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum7.TabIndex = 6
+        Me.btnNum7.Text = "7"
+        Me.btnNum7.UseVisualStyleBackColor = True
+        '
+        'btnNum6
+        '
+        Me.btnNum6.Location = New System.Drawing.Point(100, 91)
+        Me.btnNum6.Name = "btnNum6"
+        Me.btnNum6.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum6.TabIndex = 7
+        Me.btnNum6.Text = "6"
+        Me.btnNum6.UseVisualStyleBackColor = True
+        '
+        'btnNum5
+        '
+        Me.btnNum5.Location = New System.Drawing.Point(56, 91)
+        Me.btnNum5.Name = "btnNum5"
+        Me.btnNum5.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum5.TabIndex = 8
+        Me.btnNum5.Text = "5"
+        Me.btnNum5.UseVisualStyleBackColor = True
+        '
+        'btnNum4
+        '
+        Me.btnNum4.Location = New System.Drawing.Point(12, 91)
+        Me.btnNum4.Name = "btnNum4"
+        Me.btnNum4.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum4.TabIndex = 9
+        Me.btnNum4.Text = "4"
+        Me.btnNum4.UseVisualStyleBackColor = True
+        '
+        'btnNum3
+        '
+        Me.btnNum3.Location = New System.Drawing.Point(100, 126)
+        Me.btnNum3.Name = "btnNum3"
+        Me.btnNum3.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum3.TabIndex = 10
+        Me.btnNum3.Text = "3"
+        Me.btnNum3.UseVisualStyleBackColor = True
+        '
+        'btnNum2
+        '
+        Me.btnNum2.Location = New System.Drawing.Point(56, 126)
+        Me.btnNum2.Name = "btnNum2"
+        Me.btnNum2.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum2.TabIndex = 11
+        Me.btnNum2.Text = "2"
+        Me.btnNum2.UseVisualStyleBackColor = True
+        '
+        'btnNum1
+        '
+        Me.btnNum1.Location = New System.Drawing.Point(12, 126)
+        Me.btnNum1.Name = "btnNum1"
+        Me.btnNum1.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum1.TabIndex = 12
+        Me.btnNum1.Text = "1"
+        Me.btnNum1.UseVisualStyleBackColor = True
+        '
+        'btnNum0
+        '
+        Me.btnNum0.Location = New System.Drawing.Point(12, 161)
+        Me.btnNum0.Name = "btnNum0"
+        Me.btnNum0.Size = New System.Drawing.Size(38, 29)
+        Me.btnNum0.TabIndex = 13
+        Me.btnNum0.Text = "0"
+        Me.btnNum0.UseVisualStyleBackColor = True
+        '
+        'txtResultado
+        '
+        Me.txtResultado.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.txtResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtResultado.Location = New System.Drawing.Point(12, 21)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.ReadOnly = True
+        Me.txtResultado.Size = New System.Drawing.Size(170, 26)
+        Me.txtResultado.TabIndex = 15
+        Me.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnLimpar
+        '
+        Me.btnLimpar.Location = New System.Drawing.Point(56, 161)
+        Me.btnLimpar.Name = "btnLimpar"
+        Me.btnLimpar.Size = New System.Drawing.Size(38, 29)
+        Me.btnLimpar.TabIndex = 16
+        Me.btnLimpar.Text = "C"
+        Me.btnLimpar.UseVisualStyleBackColor = True
+        '
+        'btnVirgula
+        '
+        Me.btnVirgula.Location = New System.Drawing.Point(100, 162)
+        Me.btnVirgula.Name = "btnVirgula"
+        Me.btnVirgula.Size = New System.Drawing.Size(38, 29)
+        Me.btnVirgula.TabIndex = 14
+        Me.btnVirgula.Text = ","
+        Me.btnVirgula.UseVisualStyleBackColor = True
+        '
+        'btnIgual
+        '
+        Me.btnIgual.Location = New System.Drawing.Point(100, 197)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Size = New System.Drawing.Size(82, 29)
+        Me.btnIgual.TabIndex = 17
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = True
+        '
+        'frmCalculadora
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(200, 243)
+        Me.Controls.Add(Me.btnIgual)
+        Me.Controls.Add(Me.btnLimpar)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.btnVirgula)
+        Me.Controls.Add(Me.btnNum0)
+        Me.Controls.Add(Me.btnNum1)
+        Me.Controls.Add(Me.btnNum2)
+        Me.Controls.Add(Me.btnNum3)
+        Me.Controls.Add(Me.btnNum4)
+        Me.Controls.Add(Me.btnNum5)
+        Me.Controls.Add(Me.btnNum6)
+        Me.Controls.Add(Me.btnNum7)
+        Me.Controls.Add(Me.btnNum8)
+        Me.Controls.Add(Me.btnNum9)
+        Me.Controls.Add(Me.btnSoma)
+        Me.Controls.Add(Me.btnSubtracao)
+        Me.Controls.Add(Me.btnDivisao)
+        Me.Controls.Add(Me.btnMultiplicacao)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmCalculadora"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmCalculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnMultiplicacao As Button
+    Friend WithEvents btnDivisao As Button
+    Friend WithEvents btnSubtracao As Button
+    Friend WithEvents btnSoma As Button
+    Friend WithEvents btnNum9 As Button
+    Friend WithEvents btnNum8 As Button
+    Friend WithEvents btnNum7 As Button
+    Friend WithEvents btnNum6 As Button
+    Friend WithEvents btnNum5 As Button
+    Friend WithEvents btnNum4 As Button
+    Friend WithEvents btnNum3 As Button
+    Friend WithEvents btnNum2 As Button
+    Friend WithEvents btnNum1 As Button
+    Friend WithEvents btnNum0 As Button
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents btnLimpar As Button
+    Friend WithEvents btnVirgula As Button
+    Friend WithEvents btnIgual As Button
+End Class
---
+++ calculadora_basic/calculadora_basic/Calculadora.vb
@@ -0,0 +1,105 @@
+﻿Public Class frmCalculadora
+
+    Public valor As Double
+    Public funcao As String
+
+    Private Sub btnDivisao_Click(sender As Object, e As EventArgs) Handles btnDivisao.Click
+        valor = txtResultado.Text
+        txtResultado.Text = ""
+        funcao = "divide"
+    End Sub
+
+    Private Sub btnLimpar_Click(sender As Object, e As EventArgs) Handles btnLimpar.Click
+        txtResultado.Text = ""
+    End Sub
+
+    Private Sub btnMultiplicacao_Click(sender As Object, e As EventArgs) Handles btnMultiplicacao.Click
+        valor = txtResultado.Text
+        txtResultado.Text = ""
+        funcao = "multiplica"
+    End Sub
+
+    Private Sub btnNum0_Click(sender As Object, e As EventArgs) Handles btnNum0.Click
+        txtResultado.Text = txtResultado.Text & "0"
+    End Sub
+
+    Private Sub btnNum1_Click(sender As Object, e As EventArgs) Handles btnNum1.Click
+        txtResultado.Text = txtResultado.Text & "1"
+    End Sub
+
+    Private Sub btnNum2_Click(sender As Object, e As EventArgs) Handles btnNum2.Click
+        txtResultado.Text = txtResultado.Text & "2"
+    End Sub
+
+    Private Sub btnNum3_Click(sender As Object, e As EventArgs) Handles btnNum3.Click
+        txtResultado.Text = txtResultado.Text & "3"
+    End Sub
+
+    Private Sub btnNum4_Click(sender As Object, e As EventArgs) Handles btnNum4.Click
+        txtResultado.Text = txtResultado.Text & "4"
+    End Sub
+
+    Private Sub btnNum5_Click(sender As Object, e As EventArgs) Handles btnNum5.Click
+        txtResultado.Text = txtResultado.Text & "5"
+    End Sub
+
+    Private Sub btnNum6_Click(sender As Object, e As EventArgs) Handles btnNum6.Click
+        txtResultado.Text = txtResultado.Text & "6"
+    End Sub
+
+    Private Sub btnNum7_Click(sender As Object, e As EventArgs) Handles btnNum7.Click
+        txtResultado.Text = txtResultado.Text & "7"
+    End Sub
+
+    Private Sub btnNum8_Click(sender As Object, e As EventArgs) Handles btnNum8.Click
+        txtResultado.Text = txtResultado.Text & "8"
+    End Sub
+
+    Private Sub btnNum9_Click(sender As Object, e As EventArgs) Handles btnNum9.Click
+        txtResultado.Text = txtResultado.Text & "9"
+    End Sub
+
+    Private Sub btnSoma_Click(sender As Object, e As EventArgs) Handles btnSoma.Click
+        valor = txtResultado.Text
+        txtResultado.Text = ""
+        funcao = "soma"
+    End Sub
+
+    Private Sub btnSubtracao_Click(sender As Object, e As EventArgs) Handles btnSubtracao.Click
+        valor = txtResultado.Text
+        txtResultado.Text = ""
+        funcao = "subtrai"
+    End Sub
+
+    Private Sub btnVirgula_Click(sender As Object, e As EventArgs) Handles btnVirgula.Click
+        txtResultado.Text = txtResultado.Text & ","
+    End Sub
+
+    Private Sub btnIgual_Click(sender As Object, e As EventArgs) Handles btnIgual.Click
+        Select Case funcao
+            Case "soma"
+                valor = valor + txtResultado.Text
+                txtResultado.Text = valor
+            Case "subtrai"
+                valor = valor - txtResultado.Text
+                txtResultado.Text = valor
+            Case "multiplica"
+                valor = valor * txtResultado.Text
+                txtResultado.Text = valor
+            Case "divide"
+                If txtResultado.Text = "0" Then
+                    txtResultado.Text = "ERRO: divisão por 0"
+                Else
+                    valor = valor / txtResultado.Text
+                    txtResultado.Text = valor
+                End If
+
+        End Select
+
+    End Sub
+
+    Private Sub frmCalculadora_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
